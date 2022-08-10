57ef88cb97a42230f47ab362c3ca81b10e11bb05
jianshangquan <jianshangquan3@gmail.com>
2022-08-10 13:59:43 +0630

    init

---
+++ bin/amount_to_words_dart.dart
@@ -0,0 +1,5 @@
+import 'package:amount_to_words_dart/amount_to_words_dart.dart' as amount_to_words_dart;
+
+void main(List<String> arguments) {
+  print('Hello world: ${amount_to_words_dart.calculate()}!');
+}
---
+++ test/amount_to_words_dart_test.dart
@@ -0,0 +1,8 @@
+import 'package:amount_to_words_dart/amount_to_words_dart.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
