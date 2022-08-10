912d152dd632659332f47ad911d7ace078cb37e1
jianshangquan <jianshangquan3@gmail.com>
2022-08-10 14:47:25 +0630

    v0.0.0

--- bin/amount_to_words_dart.dart
+++ bin/amount_to_words_dart.dart
@@ -3,5 +3,5 @@ import 'package:amount_to_words_dart/amount_to_words_dart.dart';
 void main(List<String> arguments) {
   final output = AmountToWord.convertTo(123456789, AmountConversion.burmese);
   // output -> တစ်ထောင်နှစ်ရာသုံးဆယ့်လေးသိန်းငါးသောင်းခြှောက်ထောင်ခုနှစ်ရာရှစ်ဆယ့်ကိုးကျပ်
-  print(output);
+  print("output $output");
 }
